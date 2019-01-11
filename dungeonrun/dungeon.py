a61fbd4bb7001be4917c52977e2b723691183a22
nothrazim <kevincederqvist@gmail.com>
2019-01-11 14:59:52 +0100

    renamed room to matrix, added enter room function

--- dungeonrun/dungeon.py
+++ dungeonrun/dungeon.py
@@ -1,17 +1,37 @@
 class Map:
     def __init__(self, size):
-        if size not in [4,5,8]: raise Exception("Wrong size")
+        if size not in [4, 5, 8]:
+            raise Exception("Wrong size")
         self.size = size
-        self.room = tuple(tuple(Room(col, row) for col in range(size)) for row in range(size))
+        self.matrix = tuple(tuple(Room(col, row) for col in range(size)) for row in range(size))
 
-        corner = { 'NW': self.room[0][0], 'NE': self.room[0][size-1], 'SW': self.room[size-1][0], 'SE': self.room[size-1][size-1] }
+        corner = {'NW': self.matrix[0][0], 'NE': self.matrix[0][size-1], 'SW': self.matrix[size-1][0], 'SE': self.matrix[size-1][size-1]}
+
+    def enterDoor(self, currentRoom, door):
+        newRoom = currentRoom
+        x = currentRoom.position[0]
+        y = currentRoom.position[1]
+        if door.lower() == "west":
+            newRoom= self.matrix[x-1][y]
+        elif door.lower() == "north":
+            newRoom = self.matrix[x][y-1]
+        elif door.lower() == "east":
+            newRoom = self.matrix[x+1][y]
+        elif door.lower() == "south":
+            newRoom = self.matrix[x][y+1]
+        else:
+            raise Exception("How did you enter else?")
+        return newRoom
 
 
 class Room:
 
-    def __init__(self, col, row, isDark=True, hasExit=False,
-            monsters=[], treasures=[], doors=[]):
+    def __init__(self, row, col, isDark=True, hasExit=False,
+                 monsters=[], treasures=[], doors=[]):
         self.dark, self.hasExit = isDark, hasExit
         self.monsters, self.treasures = monsters, treasures
         self.doors = doors
         self.position = (col, row)
+
+    def getCoords(self):
+        print(self.position)
