c125dff12fdc32808ada12b486ee57d9a3a482f0
nothrazim <kevincederqvist@gmail.com>
2019-01-11 10:21:01 +0100

    Moved creation of monsters into if statements, different depending on what name one inputs for monster

--- monster.py
+++ monster.py
@@ -1,20 +1,35 @@
 
 
 class Monster:
-    def __init__(self, name, initiative, hp, attack, dexterity, rarity, position):
+    def __init__(self, name, position):
         self.name = name
-        self.initiative = initiative
-        self.hp = hp
-        self.attack = attack
-        self.dexterity = dexterity
-        self.rarity = rarity
         self.position = position
 
-    def create_skeleton(self):  # Spawn at X & Y 1
-        self.name = "Skeletor"
-        self.initiative = 4
-        self.hp = 2
-        self.attack = 3
-        self.dexterity = 3
-        self.rarity = 15
-        self.position = [1, 2]
+        if self.name.lower() == "giant spider":
+            self.initiative =7
+            self.hp = 1
+            self.attack = 2
+            self.dexterity = 3
+            self.rarity = 20
+
+        if self.name.lower() == "skeleton":
+            self.initiative = 4
+            self.hp = 2
+            self.attack = 3
+            self.dexterity = 3
+            self.rarity = 15
+
+        if self.name.lower() == "orc":
+            self.initiative = 6
+            self.hp = 3
+            self.attack = 4
+            self.dexterity = 4
+            self.rarity = 10
+
+        if self.name.lower() == "troll":
+            self.initiative = 2
+            self.hp = 4
+            self.attack = 7
+            self.dexterity = 2
+            self.rarity = 5
+
