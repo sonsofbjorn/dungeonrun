ccf545a52002e6feaa7c3693c19835c77af8092c
nothrazim <kevincederqvist@gmail.com>
2019-01-11 10:11:49 +0100

    Added monster class & function to make an instanced entity into a skeleton

--- monster.py
+++ monster.py
@@ -1,1 +1,22 @@
-print("Hello!")
+
+
+class Monster:
+    def __init__(self, name, initiative, hp, attack, dexterity, rarity, x, y):
+        self.name = name
+        self.initiative = initiative
+        self.hp = hp
+        self.attack = attack
+        self.dexterity = dexterity
+        self.rarity = rarity
+        self.x = x
+        self.y = y
+
+    def create_skeleton(self):  # Spawn at X & Y 1
+        self.name = "Skeletor"
+        self.initiative = 4
+        self.hp = 2
+        self.attack = 3
+        self.dexterity = 3
+        self.rarity = 15
+        self.x = 1
+        self.y = 1
