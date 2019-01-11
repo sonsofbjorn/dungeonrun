2203b7c6d563e976864efd252ebcbaaea5c4817c
nothrazim <kevincederqvist@gmail.com>
2019-01-11 14:59:29 +0100

    added test dudes

--- dungeonrun/main.py
+++ dungeonrun/main.py
@@ -1,5 +1,22 @@
+from dungeonrun import player
+from dungeonrun import dungeon
+
+
 def main():
     print("run something")
 
+
 if __name__ == "__main__":
     main()
+
+
+dungeon = dungeon.Map(4)
+
+dude = player.Player("Bob", "knight", dungeon.matrix[0][0])
+
+dude.show_location()
+
+newRoom = dungeon.enterDoor(dude.position, "north")
+
+dude.move_character(newRoom)
+
