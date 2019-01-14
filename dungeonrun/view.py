0a8c85724991c5f2a112afa38a5334aa44efa7ce
Sebastian Edholm <sebastian.edholm@gmail.com>
2019-01-14 14:57:16 +0100

    beginning on view

--- dungeonrun/view.py
+++ dungeonrun/view.py
@@ -1,5 +1,7 @@
 from dungeonrun import dungeon
 from dungeonrun import player
+from dungeonrun import monster
+from dungeonrun import menu
 
 class View:
 
@@ -18,3 +20,18 @@ class View:
                 else:
                     print("░", end="")
             print()
+
+    def draw_start_location_options(self):
+        startcorner = input(">>")
+        print("Choose your starting location:\n"
+                "[1] North-West\n"
+                "[2] North-East\n"
+                "[3] South-West\n"
+                "[4] South-East\n")
+        return startcorner
+
+    def draw_map_loop(self):
+        print(self.p.name + ", you are in", self.p.show_location)
+        print(self.p.name + ", where do you want to go? West, North, East, or South?")
+        inp = input(">>")
+        return inp
