bfa3723c6c190853ba12e74ad6106423cba89983
Hendrikx <mathias_hendrikx@hotmail.com>
2019-01-14 15:51:41 +0100

    Updated view

--- dungeonrun/view.py
+++ dungeonrun/view.py
@@ -9,12 +9,34 @@ class View:
 
     def draw_map(self):
         for row in self.m:
-            for room in row:
-                if room.position == self.p.show_location:
-                    print("o", end="")
+            for x in range(3):
+                for room in row:
+                    if room.position == self.p.show_location:
+                        if x == 1:
+                            print(" ", end="")
+                            print(" ╳  ", end="")
+                            print(" ", end="")
+                        else:
+                            print("   ", end="")
+                            print("", end="")
+                            print("   ", end="")
+                    elif room.hasExit and room.dark is False:
+                        if x == 1:
+                            print("░", end="")
+                            print("exit", end="")
+                            print("░", end="")
+                        else:
+                            print("░░░", end="")
+                            print("", end="")
+                            print("░░░", end="")
 
-                elif room.dark:
-                    print("▓", end="")
-                else:
-                    print("░", end="")
-            print()
+
+                    elif room.dark:
+                        print("▓▓", end="")
+                        print("▓▓", end="")
+                        print("▓▓", end="")
+                    else:
+                        print("░░", end="")
+                        print("░░", end="")
+                        print("░░", end="")
+                print()
