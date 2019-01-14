b15799b0c7118d8ff7756e150bd265828ec72141
nothrazim <kevincederqvist@gmail.com>
2019-01-14 10:52:46 +0100

    Added generate attack functions for player + monsters

--- dungeonrun/dungeon.py
+++ dungeonrun/dungeon.py
@@ -3,6 +3,7 @@ X = COL
 Y = ROW
 
 MAP EXAMPLE: SMALL 4x4
+(0, 0)(1, 0)(2, 0)(3, 0)
 (0, 1)(1, 1)(2, 1)(3, 1)
 (0, 2)(1, 2)(2, 2)(3, 2)
 (0, 3)(1, 3)(2, 3)(3, 3)
@@ -27,39 +28,47 @@ class Map:
 
         # North ->
         for x in range(size):
-            room = self.room(x, 0)
-            room.doors["N"] = False
+            room = self.get_room(x, 0)
+            room.doors["n"] = False
 
         # Down East
         for y in range(size):
-            room = self.room(size-1, y)
-            room.doors["E"] = False
+            room = self.get_room(size-1, y)
+            room.doors["e"] = False
 
         # South ->
         for x in range(size):
-            room = self.room(x, size-1)
-            room.doors["S"] = False
+            room = self.get_room(x, size-1)
+            room.doors["s"] = False
 
         for y in range(size):
-            room = self.room(0, y)
-            room.doors["W"] = False
+            room = self.get_room(0, y)
+            room.doors["w"] = False
 
     # This is here becose in matrix row is first instead of col
-    def room(self, x, y):
+    def get_room(self, x, y):
         return self.matrix[y][x]
 
     def enter_door(self, current_room, door):
         new_room = current_room
         x = current_room.position[0]
         y = current_room.position[1]
-        if door.lower() == "west":
-            new_room = self.room(x-1, y)
-        elif door.lower() == "north":
-            new_room = self.room(x, y-1)
-        elif door.lower() == "east":
-            new_room = self.room(x+1, y)
-        elif door.lower() == "south":
-            new_room = self.room(x, y+1)
+
+        direction = door.lower()[:1]
+
+        if new_room.doors.get(direction) is False:
+            print("There is no door in that direction")
+        else:
+            print("That door exists and can be entered")
+
+        if direction == "w":
+            new_room = self.get_room(x-1, y)
+        elif direction == "n":
+            new_room = self.get_room(x, y-1)
+        elif direction == "e":
+            new_room = self.get_room(x+1, y)
+        elif direction == "s":
+            new_room = self.get_room(x, y+1)
         else:
             raise Exception("How did you enter else?")
         return new_room
@@ -75,7 +84,7 @@ class Room:
         # DOORS N E S W
         self.doors = {"N": True, "E": True, "S": True, "W": True}
 
-        #self.doors = [True, True, True, True]
+        # self.doors = [True, True, True, True]
 
         # Position X,Y tuple
         self.position = (x, y)
