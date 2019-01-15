9849f16d540016d508043bedf6ba190b5b298208
Sebastian Edholm <sebastian.edholm@gmail.com>
2019-01-15 11:45:24 +0100

    fixed monsters

--- dungeonrun/monster.py
+++ dungeonrun/monster.py
@@ -2,33 +2,34 @@ import random
 
 
 class Monster:
+    available_monsters = {"giant spider": {
+                                    "initiative": 7,
+                                    "hp": 1,
+                                    "attack": 2,
+                                    "dexterity": 3,
+                                    "rarity": 20},
+                          "skeleton": {
+                                    "initiative": 4,
+                                    "hp": 2,
+                                    "attack": 2,
+                                    "dexterity": 3,
+                                    "rarity": 15},
+                          "orc": {
+                                    "initiative": 6,
+                                    "hp": 3,
+                                    "attack": 4,
+                                    "dexterity": 5,
+                                    "rarity": 10},
+                          "troll": {
+                                    "initiative": 2,
+                                    "hp": 4,
+                                    "attack": 7,
+                                    "dexterity": 2,
+                                    "rarity": 5}}
+
     def __init__(self, unit_type, room):
         self.room = room
 
-        self.available_monsters = {"giant spider": {
-                                       "initiative": 7,
-                                       "hp": 1,
-                                       "attack": 2,
-                                       "dexterity": 3,
-                                       "rarity": 20},
-                                   "skeleton": {
-                                       "initiative": 4,
-                                       "hp": 2,
-                                       "attack": 2,
-                                       "dexterity": 3,
-                                       "rarity": 15},
-                                   "orc": {
-                                       "initiative": 4,
-                                       "hp": 2,
-                                       "attack": 2,
-                                       "dexterity": 3,
-                                       "rarity": 15},
-                                   "troll": {
-                                       "initiative": 4,
-                                       "hp": 2,
-                                       "attack": 2,
-                                       "dexterity": 3,
-                                       "rarity": 15}}
 
         self.unit_type = self.available_monsters[unit_type]
 
