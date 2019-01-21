908c32dfd1ef5cf629744ea22dad1d86eb8b29af
Sebastian Edholm <sebastian.edholm@gmail.com>
2019-01-21 11:58:37 +0100

    you can now exit, no more loot on death

--- dungeonrun/controller.py
+++ dungeonrun/controller.py
@@ -93,9 +93,10 @@ class Controller:
                 dungeon_size = 8
                 break
             else:
-                self.view.print_main_menu(View.welcome_menu,
+                self.view.print_main_menu(View.choose_size,
                                           View.err_choice,
                                           error=True)
+                time.sleep(3)
         return dungeon_size
 
     def start_loc_menu(self):
@@ -119,7 +120,7 @@ class Controller:
                 start_loc = "SE"
                 break
             else:
-                self.view.print_main_menu(View.welcome_menu,
+                self.view.print_main_menu(View.choose_corner,
                                           View.err_choice,
                                           error=True)
         # This string is sent into init_objects function later
@@ -271,7 +272,8 @@ class Controller:
                 if username == uname:
                     return username, role
 
-    def init_objects(self, player, role, start_loc, dungeon_size, ai_check=False):
+    def init_objects(self, player, role, start_loc,
+                     dungeon_size, ai_check=False):
         """
         this function will init objects
         player, role, start_loc is a string.
@@ -296,7 +298,6 @@ class Controller:
         elif start_loc == "SE":
             start_room = dungeon.corner["SE"]
             dungeon.get_room(0, 0).has_exit = True
-
 
         player = Player(player, role, start_room)
 
@@ -327,8 +328,6 @@ class Controller:
         """
         while True:
             self.view.print_game(player, dungeon, View.direction_option)
-            if player.hp < 1:
-                break
 
             direction = self.view.handle_input()
             # inp = self.view.handle_input()
@@ -354,6 +353,16 @@ class Controller:
                                      View.leave_question,
                                      View.leave_options,
                                      leave_q=True)
+                usrinp = input(">>")
+                if usrinp == "1":
+                    self.exit_game()
+                    break
+                if usrinp == "2":
+                    pass
+                else:
+                    self.view.print_game(View.leave_question,
+                                         View.err_choice,
+                                         error=True)
                 time.sleep(2)
             while len(player.current_room.monsters) > 0:
                 self.view.print_game(player,
@@ -364,9 +373,19 @@ class Controller:
                 time.sleep(3)
                 while len(player.current_room.monsters) > 0:
                     self.combat(player, dungeon)
+
+            if player.hp < 1:
+                break
+
             while len(player.current_room.treasures) > 0:
                 self.loot(player, dungeon)
 
+    def exit_game(self):
+        self.print_statistics
+        quit()
+
+    def print_statistics(self):
+        print("end screen")
 
     def loot(self, player, dungeon):
         """
@@ -473,6 +492,13 @@ class Controller:
         return True
 
     def move_player(self, player, direction, dungeon_map):
+        """
+        This moves a player object on the map
+        with directions, N, E, S, W as strings
+        It then returns a new room object.
+        If the player fails in moving it returns
+        a False
+        """
         x = player.current_room.position[0]
         y = player.current_room.position[1]
 
@@ -521,7 +547,7 @@ class Controller:
                 if defender.hero_class == "knight":
                     if defender.block is True:
                         defender.block = False
-                        return View.monster_hit, attacker.unit_type, View.shield_block
+                        return View.shield_block, attacker.unit_type
                     else:
                         defender.hp -= 1
                         return View.monster_hit, attacker.unit_type, View.for_one_dmg
@@ -533,7 +559,6 @@ class Controller:
                 return View.player_miss, defender.unit_type
             else:
                 return attacker.unit_type, View.monster_miss
-
 
     def roll_dice(self, user, dice_type):
         if dice_type == "attack":
