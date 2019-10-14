995db6f9e0c07f5a65479ea73dab3cbc3f149ba3
Sebastian Edholm <sebastian.eholm@gmailcom>
2019-10-14 12:53:37 +0200

    REMOVED DJANGO

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3
-RUN mkdir /dungeonrun
-WORKDIR /dungeonrun
-COPY requirements.txt /dungeonrun
-RUN pip install -r requirements.txt
-COPY . /dungeonrun
-
-EXPOSE 8080/tcp
--- game/web_view.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from .dungeon import Map
-
-
-def index(request):
-    dungeon = Map(4)
-    monsters = dungeon.print_monsters()
-    return HttpResponse(monsters)
